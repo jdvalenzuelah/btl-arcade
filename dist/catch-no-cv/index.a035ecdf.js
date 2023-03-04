215457efffc52a5a475fddf16017687782b40eb7
Josue Valenzuela <val171001@uvg.edu.gt>
2023-03-03 19:06:56 -0600

    release3

--- dist/catch-no-cv/index.a035ecdf.js
+++
@@ -1,2 +0,0 @@
-const config={catcherSize:[200,200],livesSize:[80,80],fallingItemsSize:[150,150],game:{lives:3,fallingItems:{initial:2,step:1,max:5,levelUpStep:1,newSpawnOffsetMs:400},fallSpeed:{initial:10,step:1,max:30},levelUpMs:2e3,winScore:30},loseCooldownMs:1e4,winCooldownMs:1e4,camera:{size:[1280,720],sx:400,sy:0,sw:480,sh:360},override:!0};
-//# sourceMappingURL=index.a035ecdf.js.map
